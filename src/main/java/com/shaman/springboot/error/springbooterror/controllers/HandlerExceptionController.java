54abf0a7ec34d2fff4b748b12cb3f10f29ec248b
Elshaman <cbuitragoo.2023@gmail.com>
2023-12-28 10:39:57 -0500

    3_error_recurso_no_encontrado

--- src/main/java/com/shaman/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/shaman/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.shaman.springboot.error.springbooterror.models.Error;
 
@@ -21,6 +22,16 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         //return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException ex){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error division por cero");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(404).body(error);
     }
 
 }

f65de1d17ccf62a5c4841d15fb834264650d1626
Elshaman <cbuitragoo.2023@gmail.com>
2023-12-28 09:40:58 -0500

    2_primer_metodo_manejo_Excepciones

--- src/main/java/com/shaman/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/shaman/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -1,5 +1,8 @@
 package com.shaman.springboot.error.springbooterror.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,8 +13,14 @@ import com.shaman.springboot.error.springbooterror.models.Error;
 public class HandlerExceptionController {
 
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> divisionByZero(Exception ex){
-        return ResponseEntity.internalServerError().body("error division zero 500");
+    public ResponseEntity<Error> divisionByZero(Exception ex){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error division por cero");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        //return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
 }
